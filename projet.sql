c5f1933ede13eae8a7b3fcf731ad9a2d9b356b88
houcimi <30775212+houcimi@users.noreply.github.com>
2023-04-12 23:35:00 +0200

    Update projet.sql

--- projet.sql
+++ projet.sql
@@ -1,8 +1,15 @@
-Drop table Campuses;
-Drop table Equipment;
-Drop table Users;
-Drop table Tickets;
-Drop table Software;
+ DROP TABLE IF EXISTS Tickets;
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Campuses;
+DROP TABLE IF EXISTS Software;
+DROP TABLE IF EXISTS Equipment;
+DROP SEQUENCE IF EXISTS equipId;
+DROP SEQUENCE IF EXISTS campusId;
+DROP SEQUENCE IF EXISTS userId;
+DROP SEQUENCE IF EXISTS TicketId;
+DROP SEQUENCE IF EXISTS softId;
+
+
 ---- TABLE Campuses -----
 CREATE TABLE Campuses (
   id INT PRIMARY KEY,
@@ -12,18 +19,6 @@ CREATE TABLE Campuses (
   region VARCHAR(255),
   codepostal VARCHAR(255)
 );
----- TABLE Equipment -----
-CREATE TABLE Equipment (
-  id INT PRIMARY KEY,
-  name VARCHAR(255),
-  serial_number VARCHAR(255),
-  purchase_date DATE,
-  purchase_cost DECIMAL(10,2),
-  warranty_expiry_date DATE,
-  campus_id INT,
-  FOREIGN KEY (campus_id) REFERENCES Campuses(id)
-);
-
 
 ---- TABLE Users -----
 CREATE TABLE Users (
@@ -59,129 +54,230 @@ CREATE TABLE Software (
   FOREIGN KEY (campus_id) REFERENCES Campuses(id)
 );
 
----- Procedure that insert an equipment in the database -----
-CREATE PROCEDURE insertEquipment(
-  IN name VARCHAR(255),
-  IN serial_number VARCHAR(255),
-  IN purchase_date DATE,
-  IN purchase_cost DECIMAL(10,2),
-  IN warranty_expiry_date DATE,
-  IN campus_id INT
-)
+---- TABLE Equipment -----
+CREATE TABLE Equipment (
+  id INT PRIMARY KEY,
+  name VARCHAR(255),
+  serial_number VARCHAR(255),
+  purchase_date DATE,
+  purchase_cost DECIMAL(10,2),
+  warranty_expiry_date DATE,
+  campus_id INT,
+  FOREIGN KEY (campus_id) REFERENCES Campuses(id)
+);
+--------  Function return  
+CREATE OR REPLACE FUNCTION check_record_exists(p_id IN NUMBER, p_table_name IN VARCHAR2)
+RETURN NUMBER
+IS
+  v_count NUMBER;
 BEGIN
-  INSERT INTO Equipment (name, serial_number, purchase_date, purchase_cost, warranty_expiry_date, campus_id)
-  VALUES (name, serial_number, purchase_date, purchase_cost, warranty_expiry_date, campus_id);
+  -- perform the count query
+  EXECUTE IMMEDIATE 'SELECT COUNT(*) FROM ' || p_table_name || ' WHERE id = :1' INTO v_count USING p_id;
+  
+  -- if count is 0, return 0, otherwise return 1
+  IF v_count = 0 THEN
+    RETURN 0;
+  ELSE
+    RETURN 1;
+  END IF;
+EXCEPTION
+  -- handle any errors that may occur
+  WHEN OTHERS THEN
+    RETURN 0;
+END;
+---- Procedure that insert an equipment in the database -----
+CREATE SEQUENCE equipId START WITH 1 INCREMENT BY 1 ;
+CREATE OR REPLACE PROCEDURE insertEquipment(
+  pname IN  VARCHAR,
+  pserial_number IN  VARCHAR,
+  ppurchase_date IN  DATE,
+  ppurchase_cost IN  DECIMAL(10,2),
+  pwarranty_expiry_date IN  DATE,
+  pcampus_id IN  INT
+)
+IS
+idEquip NUMBER;
+BEGIN
+  SELECT equipId.NEXTVAL INTO idEquip FROM dual;
+  INSERT INTO Equipment (id,name, serial_number, purchase_date, purchase_cost, warranty_expiry_date, campus_id)
+  VALUES (idEquip,pname, pserial_number, ppurchase_date, ppurchase_cost, pwarranty_expiry_date, pcampus_id);
 END;
 ---- Procedure that insert a campus in the database -----
-CREATE PROCEDURE insertCampus(
-  IN name VARCHAR(255),
-  IN address VARCHAR(255),
-  IN ville VARCHAR(255),
-  IN region VARCHAR(255),
-  IN codepostal VARCHAR(255)
+CREATE SEQUENCE campusId START WITH 1 INCREMENT BY 1 ;
+CREATE OR REPLACE PROCEDURE insertCampus(
+pname IN VARCHAR,
+address IN VARCHAR,
+ville IN VARCHAR,
+region IN VARCHAR,
+codepostal IN VARCHAR
 )
+IS
+  name_exists NUMBER;
+  idCampus NUMBER;
 BEGIN
-  INSERT INTO Campuses (name, address, ville, region, codepostal)
-  VALUES (name, address, ville, region, codepostal);
+    SELECT campusId.NEXTVAL INTO idCampus FROM dual;
+ SELECT COUNT(*) INTO name_exists FROM Campuses WHERE name = pname;
+ IF name_exists > 0 THEN
+    RAISE_APPLICATION_ERROR(-20001, 'A site Avec le meme nom Deja exists.');
+ELSE
+INSERT INTO Campuses (id,name, address, ville, region, codepostal)
+VALUES (idCampus,pname, address, ville, region, codepostal);
+  END IF;
+
 END;
 ----- Procedure that insert a user in the database and check if the user already exist  THROW an exception -----
+CREATE SEQUENCE userId START WITH 1 INCREMENT BY 1 ;
 CREATE PROCEDURE insertUser(
-  IN name VARCHAR(255),
-  IN email VARCHAR(255),
-  IN password VARCHAR(255),
-  IN campus_id INT
+  pname IN  VARCHAR(255),
+  email IN  VARCHAR(255),
+  password IN  VARCHAR(255),
+  campus_id IN  INT
 )
+IS
+user_id INT;
+idUser NUMBER;
 BEGIN
-  DECLARE user_id INT;
-  SELECT id INTO user_id FROM Users WHERE name = name AND email = email AND password = password AND campus_id = campus_id;
+ SELECT userId.NEXTVAL INTO idUser FROM dual;
+  SELECT id INTO user_id FROM Users WHERE name = pname AND email = email AND campus_id = campus_id;
   IF user_id IS NULL THEN
-    INSERT INTO Users (name, email, password, campus_id)
-    VALUES (name, email, password, campus_id);
+    INSERT INTO Users (id,name, email, password, campus_id)
+    VALUES (idUser,pname, email, password, campus_id);
   ELSE
-    ------ EXCEPTION A COMPLETE 'User already in the database';
+    RAISE_APPLICATION_ERROR(-20002, 'ce utilisateur deja exist Deja exists.');
   END IF;
 END;
 ----- Procedure that insert a ticket in the database and check if the ticket is already in the database throw an exception -----
+CREATE SEQUENCE TicketId START WITH 1 INCREMENT BY 1 ;
 CREATE PROCEDURE insertTicket(
-  IN title VARCHAR(255),
-  IN description VARCHAR(255),
-  IN status VARCHAR(255),
-  IN priority VARCHAR(255),
-  IN user_id INT,
-  IN campus_id INT
+  ptitle IN  VARCHAR,
+  pdescription IN  VARCHAR,
+  pstatus IN  VARCHAR,
+  priorityp IN  VARCHAR,
+  puser_id IN  INT,
+  pcampus_id IN  INT
 )
+IS
+ticket_id INT;
+idTicket NUMBER;
 BEGIN
-  DECLARE ticket_id INT;
-  SELECT id INTO ticket_id FROM Tickets WHERE title = title AND description = description AND status = status AND priority = priority AND user_id = user_id AND campus_id = campus_id;
-  IF ticket_id IS NULL THEN
-    INSERT INTO Tickets (title, description, status, priority, user_id, campus_id)
-    VALUES (title, description, status, priority, user_id, campus_id);
-  ELSE
-    ------ EXCEPTION A COMPLETE 'Ticket already in the database';
-  END IF;
+  SELECT TicketId.NEXTVAL INTO idTicket FROM dual;
+    INSERT INTO Tickets (id,title, description, status, priority, user_id, campus_id)
+    VALUES (idTicket,ptitle, pdescription, pstatus, priorityp, puser_id, pcampus_id);
 END;
 ---- Procedure that insert a software in the database -----	
+CREATE SEQUENCE softId START WITH 1 INCREMENT BY 1 ;
+
 CREATE PROCEDURE insertSoftware(
-  IN name VARCHAR(255),
-  IN version VARCHAR(255),
-  IN licence_number VARCHAR(255),
-  IN licence_expiry_date DATE,
-  IN campus_id INT
+  pname IN  VARCHAR,
+  pversion IN  VARCHAR,
+  plicence_number IN  VARCHAR,
+  plicence_expiry_date IN  DATE,
+  pcampus_id IN  INT
 )
+IS
+idSoftware NUMBER;
 BEGIN
-  INSERT INTO Software (name, version, licence_number, licence_expiry_date, campus_id)
-  VALUES (name, version, licence_number, licence_expiry_date, campus_id);
+  SELECT softId.NEXTVAL INTO idSoftware FROM dual;
+  INSERT INTO Software (id,name, version, licence_number, licence_expiry_date, campus_id)
+  VALUES (idSoftware,pname, pversion, plicence_number, plicence_expiry_date, pcampus_id);
 END;
 ----- Procedure that update an equipment in the database -----
 CREATE PROCEDURE updateEquipment(
-  IN id INT,
-  IN name VARCHAR(255),
-  IN serial_number VARCHAR(255),
-  IN purchase_date DATE,
-  IN purchase_cost DECIMAL(10,2),
-  IN warranty_expiry_date DATE,
-  IN campus_id INT
+  pid IN  INT,
+  pname IN  VARCHAR,
+  pserial_number IN  VARCHAR,
+  ppurchase_date IN  DATE,
+  ppurchase_cost IN  DECIMAL(10,2),
+  pwarranty_expiry_date IN  DATE,
+  pcampus_id IN  INT
 )
+IS
+equipment_count INT;
 BEGIN
-  UPDATE Equipment SET name = name, serial_number = serial_number, purchase_date = purchase_date, purchase_cost = purchase_cost, warranty_expiry_date = warranty_expiry_date, campus_id = campus_id WHERE id = id;
+  SELECT COUNT(*) INTO equipment_count FROM Equipment WHERE id = pid;
+  IF equipment_count = 0 THEN
+    RAISE_APPLICATION_ERROR(-20003, 'Équipement inexistant.');
+  END IF;
+  UPDATE Equipment SET name = pname, serial_number = pserial_number, purchase_date = ppurchase_date, purchase_cost = ppurchase_cost, warranty_expiry_date = pwarranty_expiry_date, campus_id = pcampus_id WHERE id = pid;
 END;
 ----- Procedure that update a campus in the database -----
 CREATE PROCEDURE updateCampus(
-  IN id INT,
-  IN name VARCHAR(255),
-  IN address VARCHAR(255),
-  IN ville VARCHAR(255),
-  IN region VARCHAR(255),
-  IN codepostal VARCHAR(255)
+ pid IN INT,
+  pname IN VARCHAR,
+  address IN VARCHAR,
+  ville IN VARCHAR,
+  region IN VARCHAR,
+  codepostal IN VARCHAR
 )
+IS
+campus_count INT;
 BEGIN
-  UPDATE Campuses SET name = name, address = address, ville = ville, region = region, codepostal = codepostal WHERE id = id;
+  SELECT COUNT(*) INTO campus_count FROM Campuses WHERE id = pid;
+  IF campus_count = 0 THEN
+    RAISE_APPLICATION_ERROR(-20004, 'Campus inexistant.');
+  END IF;
+  UPDATE Campuses SET name = pname, address = address, ville = ville, region = region, codepostal = codepostal WHERE id = pid;
 END;
 ----- Procedure that update a user in the database -----
 CREATE PROCEDURE updateUser(
-  IN id INT,
-  IN name VARCHAR(255),
-  IN email VARCHAR(255),
-  IN password VARCHAR(255),
-  IN campus_id INT
+  pid IN INT,
+  pname IN VARCHAR,
+  email IN VARCHAR,
+  ppassword IN VARCHAR,
+  pcampus_id IN INT
 )
+IS
+user_count INT;
 BEGIN
-  UPDATE Users SET name = name, email = email, password = password, campus_id = campus_id WHERE id = id;
+  SELECT COUNT(*) INTO user_count FROM Users WHERE id = pid;
+  IF user_count = 0 THEN
+    RAISE_APPLICATION_ERROR(-20005, 'Utilisateur inexistant.');
+  END IF;
+  UPDATE Users SET name = pname, email = email, password = ppassword, campus_id = pcampus_id WHERE id = pid;
+END;
+---- function that check if the ticket exist or not if not return 0 else return 1 -----
+CREATE OR REPLACE FUNCTION checkTicketExist(
+  pid IN INT
+)
+RETURN INT
+IS
+ticket_count INT;
+BEGIN
+  SELECT COUNT(*) INTO ticket_count FROM Tickets WHERE id = pid;
+  IF ticket_count = 0 THEN
+    RETURN 0;
+  ELSE
+    RETURN 1;
+  END IF;
 END;
 ----- Procedure that update a ticker status in the database -----
 CREATE PROCEDURE updateTicketStatus(
-  IN id INT,
-  IN status VARCHAR(255)
+ 
+ pid IN INT,
+  pstatus IN VARCHAR
 )
+IS
+ticketExist INT;
 BEGIN
-  UPDATE Tickets SET status = status WHERE id = id;
+  SELECT checkTicketExist(pid) INTO ticketExist FROM dual;
+  IF ticketExist = 0 THEN
+    RAISE_APPLICATION_ERROR(-20006, 'Ticket inexistant.');
+  END IF;
+  UPDATE Tickets SET status = pstatus WHERE id = Pid;
 END;
+
 ----- Procedure that update a ticket priority in the database -----
 CREATE PROCEDURE updateTicketPriority(
-  IN id INT,
-  IN priority VARCHAR(255)
+  pid IN INT,
+  ppriority IN VARCHAR
 )
+IS
+ticketExist INT;
 BEGIN
+  SELECT checkTicketExist(pid) INTO ticketExist FROM dual;
+  IF ticketExist = 0 THEN
+    RAISE_APPLICATION_ERROR(-20007, 'Ticket inexistant.');
+  END IF;
   UPDATE Tickets SET priority = priority WHERE id = id;
 END;
 ----- Procedure that update a software in the database -----
@@ -198,66 +294,76 @@ BEGIN
 END;
 ----- Procedure that delete an equipment before deleting check if exist or not if not throw exception -----
 CREATE PROCEDURE deleteEquipment(
-  IN id INT
+  pid IN INT
 )
-------  A COMPLETE
+IS
+equipment_count INT;
+BEGIN
+  SELECT COUNT(*) INTO equipment_count FROM Equipment WHERE id = pid;
+  IF equipment_count = 0 THEN
+    RAISE_APPLICATION_ERROR(-20008, 'Équipement inexistant.');
+  END IF;
+  DELETE FROM Equipment WHERE id = pid;
+END;
+
 
 ----- Procedure that delete a campus before deleting check if exist or not if not throw exception -----
 CREATE PROCEDURE deleteCampus(
-  IN id INT
+ pid IN INT
 )
+IS
+campus_count INT;
 BEGIN
-  DECLARE campus_id INT;
-  SELECT id INTO campus_id FROM Campuses WHERE id = id;
-  IF campus_id IS NULL THEN
-    ------ EXCEPTION A COMPLETE 'Campus does not exist';
-  ELSE
-    DELETE FROM Campuses WHERE id = id;
+  SELECT COUNT(*) INTO campus_count FROM Campuses WHERE id = pid;
+  IF campus_count = 0 THEN
+    RAISE_APPLICATION_ERROR(-20009, 'Campus inexistant.');
   END IF;
+  DELETE FROM Campuses WHERE id = pid;
 END;
 ----- Procedure that delete a user before deleting check if exist or not if not throw exception -----
 CREATE PROCEDURE deleteUser(
-  IN id INT
+  pid IN INT
 )
+IS
+user_count INT;
 BEGIN
-  DECLARE user_id INT;
-  SELECT id INTO user_id FROM Users WHERE id = id;
-  IF user_id IS NULL THEN
-    ---- EXCEPTION A COMPLETE 'User does not exist';
-  ELSE
-    DELETE FROM Users WHERE id = id;
+  SELECT COUNT(*) INTO user_count FROM Users WHERE id = pid;
+  IF user_count = 0 THEN
+    RAISE_APPLICATION_ERROR(-20010, 'Utilisateur inexistant.');
   END IF;
+  DELETE FROM Users WHERE id = pid;
 END;
 ----- Procedure that delete a ticket before deleting check if exist or not if not throw exception -----
 CREATE PROCEDURE deleteTicket(
-  IN id INT
+  pid IN INT
 )
+IS
+ticket_count INT;
 BEGIN
-  DECLARE ticket_id INT;
-  SELECT id INTO ticket_id FROM Tickets WHERE id = id;
-  IF ticket_id IS NULL THEN
-    ------ EXCEPTION A COMPLETE 'Ticket does not exist';
-  ELSE
-    DELETE FROM Tickets WHERE id = id;
+  SELECT COUNT(*) INTO ticket_count FROM Tickets WHERE id = pid;
+  IF ticket_count = 0 THEN
+    RAISE_APPLICATION_ERROR(-20011, 'Ticket inexistant.');
   END IF;
+  DELETE FROM Tickets WHERE id = pid;
 END;
 ----- Procedure that delete a software before deleting check if exist or not if not throw exception -----
 CREATE PROCEDURE deleteSoftware(
-  IN id INT
+  pid IN INT
 )
-
+IS
+software_count INT;
+BEGIN
+  SELECT COUNT(*) INTO software_count FROM Software WHERE id = pid;
+  IF software_count = 0 THEN
+    RAISE_APPLICATION_ERROR(-20012, 'Logiciel inexistant.');
+  END IF;
+  DELETE FROM Software WHERE id = pid;
+END;
 
 
 ------ admin role that have all the privileges on the tables and procedures in the database ------
 CREATE ROLE admin;
 GRANT ALL PRIVILEGES ON Equipment TO admin;
-
----- Create supportIT 
-
----- Etudient role have read only access to Tickets and Users tables ------
-CREATE ROLE Etudient;
-GRANT SELECT ON Tickets TO Etudient;
-GRANT SELECT ON Users TO Etudient;
 
 ----  view that show all the tickets and the name of the user that created the ticket ------
 CREATE VIEW TicketsUsers AS
@@ -286,56 +392,57 @@ CREATE TRIGGER checkTicketStatus BEFORE DELETE ON Tickets
 FOR EACH ROW
 BEGIN
   IF OLD.status != 'Closed' THEN
-    ------ EXCEPTION A COMPLETE 'Ticket status is not closed';
+     raise_application_error(-20114,' LE STATUS DU Ticket status is pas closed');
   END IF;
 END;
 ----- Trigger that on creating a ticket check for the user campus if the campus is not the same as the ticket campus throw an exception ------
-CREATE TRIGGER checkTicketCampus BEFORE INSERT ON Tickets
+
+CREATE OR REPLACE TRIGGER checkTicketCampus
+BEFORE INSERT ON Tickets
 FOR EACH ROW
+DECLARE 
+  user_campus_id Users.campus_id%TYPE;
+  ticket_campus_id Tickets.campus_id%TYPE;
 BEGIN
-  DECLARE user_campus_id INT;
-  DECLARE ticket_campus_id INT;
-  SELECT campus_id INTO user_campus_id FROM Users WHERE id = NEW.user_id;
-  SELECT campus_id INTO ticket_campus_id FROM Tickets WHERE id = NEW.id;
+  SELECT campus_id INTO user_campus_id FROM Users WHERE id = :NEW.user_id;
+  SELECT campus_id INTO ticket_campus_id FROM Tickets WHERE id = :NEW.id;
   IF user_campus_id != ticket_campus_id THEN
-    ------ EXCEPTION A COMPLETE 'User campus is not the same as the ticket campus';
+    raise_application_error(-20115, 'le campus de lutilisateur est different du campus du ticket');
   END IF;
 END;
 ----- function that return the number of tickets created by a user ------
-CREATE FUNCTION getNumberOfTicketsByUser(
-  IN user_id INT
+CREATE OR REPLACE FUNCTION getNumberOfTicketsByUser(
+  user_id IN INT
 )
-RETURNS INT
+RETURN INT IS
+  numberOfTickets INT;
 BEGIN
-  DECLARE numberOfTickets INT;
   SELECT COUNT(*) INTO numberOfTickets FROM Tickets WHERE user_id = user_id;
   RETURN numberOfTickets;
 END;
 ----- function that return the number of tickets created by a user in a specific campus ------
-CREATE FUNCTION getNumberOfTicketsByUserAndCampus(
-  IN user_id INT,
-  IN campus_id INT
+CREATE OR REPLACE FUNCTION getNumberOfTicketsByUserAndCampus(
+  user_id IN INT,
+  campus_id IN INT
 )
-RETURNS INT
+RETURN INT IS
+  numberOfTickets INT;
 BEGIN
-  DECLARE numberOfTickets INT;
   SELECT COUNT(*) INTO numberOfTickets FROM Tickets WHERE user_id = user_id AND campus_id = campus_id;
   RETURN numberOfTickets;
 END;
 -----  function that return the average purchase cost of all equipments in a campus ------	
-CREATE FUNCTION getAveragePurchaseCostByCampus(
-  IN campus_id INT
+CREATE OR REPLACE FUNCTION getAveragePurchaseCostByCampus(
+  campus_id IN INT
 )
-RETURNS DECIMAL(10,2)
+RETURN DECIMAL(10,2) IS
+  averagePurchaseCost DECIMAL(10,2);
 BEGIN
-  DECLARE averagePurchaseCost DECIMAL(10,2);
   SELECT AVG(purchase_cost) INTO averagePurchaseCost FROM Equipment WHERE campus_id = campus_id;
   RETURN averagePurchaseCost;
 END;
 
 -----  Distributed database that store data for each campus in a different server ------
-CREATE DATABASE IF NOT EXISTS Campus1;
-CREATE DATABASE IF NOT EXISTS Campus2;
 
 -----  query plan that shows the number of tickets in each campus ------
 EXPLAIN SELECT COUNT(*), Campuses.name FROM Tickets INNER JOIN Campuses ON Tickets.campus_id = Campuses.id GROUP BY Tickets.campus_id;
